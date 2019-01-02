fa84ce83a319e008b876624ac6d7d913f1ffd20d
tetras92 <manuelamous@gmail.com>
2019-01-02 13:36:56 +0100

    version 2

---
+++ Modele.py
@@ -0,0 +1,124 @@
+import csv
+class Modele:
+
+    def __init__(self, fichier_alternatives):
+        self.A = dict() # dictionnaire des points alternatives
+        self.I = dict() # point ideal
+        self.N = dict() # point nadir approche
+        self.X_star = dict()  # Dictionnaire : critere -> alternative optimisant ledit critere. Utilise pour la determination du nadir approche.
+        with open(fichier_alternatives) as csvfile:
+            self.base_lignes_alternatives = csv.DictReader(csvfile, delimiter=',')
+            self.initialize_I_N_X_star()
+            for ligne_alternative in self.base_lignes_alternatives:
+                modele = ligne_alternative["md"]
+                del ligne_alternative["md"]
+                self.A[modele] = {f : int(value) for f, value in ligne_alternative.items()}
+
+
+            self.E = self.A.keys()
+
+    def initialize_I_N_X_star(self):
+        self.I = {fieldname: None for fieldname in set(self.base_lignes_alternatives.fieldnames) - {"md"}}
+        self.X_star = {fieldname: None for fieldname in set(self.base_lignes_alternatives.fieldnames) - {"md"}}
+        self.N = {fieldname: list() for fieldname in set(self.base_lignes_alternatives.fieldnames) - {"md"}}
+
+    def compute_I_and_N(self):
+        self.initialize_I_N_X_star()
+        for alternative_id in self.E:
+            for fieldname in self.X_star:
+                if self.I[fieldname] == None or self.I[fieldname] > int(self.A[alternative_id][fieldname]):
+                    self.I[fieldname] = self.A[alternative_id][fieldname]
+                    self.X_star[fieldname] = alternative_id
+
+        #calcul du point nadir approche
+        for fieldname, x in self.X_star.items():
+            for f, value in self.A[x].items():
+                self.N[f].append(value)
+        self.N = {fieldname : max(L) for fieldname, L in self.N.items()}
+
+
+    def upload_criteria_weight(self, weight_file):
+        with open(weight_file) as csvfile:
+            ligne_poids = csv.DictReader(csvfile, delimiter=',')
+            self.W = ligne_poids.next()
+            self.W = {f : float(value) for f,value in self.W.items()}
+
+
+    def nearest_alternative_to_I(self, weight_file="weights_file.csv", epsilon=0.1):
+        self.E = self.A.keys()  #Reconsideration de l'ensemble des alternatives
+        self.compute_I_and_N()
+        self.upload_criteria_weight(weight_file=weight_file)
+        #--- CALCUL DU VECTEUR LAMBDA ---#
+        Lambda = dict()
+        for fieldname, nadValue in self.N.items():
+            Lambda[fieldname] = self.W[fieldname]/(nadValue - self.I[fieldname])
+        #-------------------------------------------#
+        #---DETERMINATION DE LA MEILLEURE SOLUTION---#
+
+        L = list() #liste des couples modele, valeur de la norme de Tchebycheff
+        for modele, composantes in self.A.items():
+            if modele in self.E:
+                max_value = None
+                noise_expr = 0
+                for i, x_i in composantes.items():
+                    value = Lambda[i]*(x_i - self.I[i])
+                    if max_value == None or value > max_value:
+                        max_value = value
+                    noise_expr += value
+
+
+                L.append((modele, max_value + epsilon * noise_expr))
+        L.sort(key=lambda c:c[1])
+        print(L)
+        self.O = L[0][0]        # solution optimale
+        return L[0]
+
+    def set_criteria_to_improve(self, criteria="eu"):
+        value_of_this_criteria_in_current_opt = self.A[self.O][criteria]
+        #rappel : minimization
+        self.E = {a for a, composantes in self.A.items() if composantes[criteria] < value_of_this_criteria_in_current_opt}
+
+    def upload_performance_vector(self, file="performance_cible.csv"):
+        with open(file) as csvfile:
+            ligne_performance = csv.DictReader(csvfile, delimiter=',')
+            self.V_P = ligne_performance.next()
+            self.V_P = {f : float(value) for f,value in self.V_P.items()}
+
+    def nearest_alternative_to_V_P(self, weight_file="weights_file.csv", performance_file="performance_cible.csv", epsilon=0.1):
+        self.upload_performance_vector(file=performance_file)
+        print(self.V_P)
+        self.compute_I_and_N()
+        # print(self.N)
+        self.upload_criteria_weight(weight_file=weight_file)
+        # --- CALCUL DU VECTEUR LAMBDA ---#
+        Lambda = dict()
+        for fieldname, nadValue in self.N.items():
+            Lambda[fieldname] = self.W[fieldname] / (nadValue - self.I[fieldname])
+        # -------------------------------------------#
+        # ---DETERMINATION DE LA MEILLEURE SOLUTION---#
+        L = list()  # liste des couples modele, valeur de la norme de Tchebycheff
+        for modele, composantes in self.A.items():
+            if modele in self.E:
+                max_value = None
+                noise_expr = 0
+                for i, x_i in composantes.items():
+                    value = Lambda[i] * (x_i - self.V_P[i])
+                    if max_value == None or value > max_value:
+                        max_value = value
+                    noise_expr += value
+
+                L.append((modele, max_value + epsilon * noise_expr))
+        L.sort(key=lambda c: c[1])
+        print(L)
+        self.PP = L[0][0]  #solution la plus proche du vecteur de performance fourni
+        print(self.A[self.PP])
+        return L[0]
+
+if __name__ == '__main__':
+    m = Modele('voitures.csv')
+    print("Ensemble des alternatives", m.E)
+    # print(m.nearest_alternative_to_I(weight_file="weights_file.csv", epsilon=0.1))
+    # m.set_criteria_to_improve(criteria="cv")
+    # print("Ensemble des alternatives", m.E)
+    # print(m.nearest_alternative_to_I(weight_file="weights_file.csv", epsilon=0.1))
+    print(m.nearest_alternative_to_V_P())
---
+++ main.py
@@ -0,0 +1,70 @@
+import csv
+
+A = dict() # dictionnaire des points alternatives
+L = list() #Liste des voitures
+I = dict() #point ideal
+N = dict() #point nadir approche
+
+epsilon = 0.1
+
+# #---CHARGEMENT DE FICHIER AVEC CALCUL DU POINT NADIR (APPROCHE) ET IDEAL---#
+# -----------------------------------------------------------------------#
+with open('voitures.csv') as csvfile:
+    base_lignes_voiture = csv.DictReader(csvfile, delimiter=',')
+    I = {fieldname : None for fieldname in set(base_lignes_voiture.fieldnames) - {"md"}}
+    X_star = {fieldname : None for fieldname in set(base_lignes_voiture.fieldnames) - {"md"}}
+    N = {fieldname : list() for fieldname in set(base_lignes_voiture.fieldnames) - {"md"}}
+    for voiture in base_lignes_voiture:
+        modele = voiture["md"]
+        del voiture["md"]
+        A[modele] = {f : int(value) for f, value in voiture.items()}
+
+        # calcul point ideal
+        for fieldname in X_star:
+            if I[fieldname] == None or I[fieldname] > int(voiture[fieldname]):
+                I[fieldname] = A[modele][fieldname]
+                X_star[fieldname] = modele
+    #calcul du point nadir approche
+    for fieldname, x in X_star.items():
+        for f, value in A[x].items():
+            N[f].append(value)
+    N = {fieldname : max(L) for fieldname, L in N.items()}
+
+
+print("Nadir" , N)
+print("IDEAL", I)
+
+#-----------------------------------------------------------------------#
+#---CALCUL DE LA SOLUTION LA PLUS PROCHE DU POINT IDEAL DANS LA DIRECTION DU POINT NADIR---#
+W = dict() #dictionnaire des poids de criteres
+with open('weights_file.csv') as csvfile:
+    ligne_poids = csv.DictReader(csvfile, delimiter=',')
+    W = ligne_poids.next()
+    W = {f : float(value) for f,value in W.items()}
+print("vecteur de poids", W)
+
+#----------------------------------------------------------------------#
+#--- CALCUL DU VECTEUR LAMBDA ---#
+Lambda = dict()
+for fieldname, nadValue in N.items():
+    Lambda[fieldname] = W[fieldname]/(nadValue - I[fieldname])
+print("Lambda ", Lambda)
+#--------------------------------------------------------------------#
+#---DETERMINATION DE LA MEILLEURE SOLUTION---#
+
+L = list() #liste des couples modele, valeur de la norme de Tchebycheff
+for modele, composantes in A.items():
+    max_value = None
+    noise_expr = 0
+    for i, x_i in composantes.items():
+        value = Lambda[i]*abs(x_i - I[i])
+        if max_value == None or value > max_value:
+            max_value = value
+        noise_expr += value
+
+
+    L.append((modele, max_value + epsilon * noise_expr))
+
+
+L.sort(key=lambda c:c[1])
+print(L[0][0], A[L[0][0]])
